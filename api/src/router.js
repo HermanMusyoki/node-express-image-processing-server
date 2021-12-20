c539eb524ccde08153cfdc194b6edea02ac497c2
HermanMusyoki <salmantso14@gmail.com>
2021-12-20 04:14:43 +0000

    add worker threads

--- api/src/router.js
+++ api/src/router.js
@@ -1,8 +1,11 @@
 const { Router } = require('express');
 const router = Router();
 const multer = require ('multer');
+const path = require('path');
+
 const storage = multer.diskStorage({destination : 'api/uploads/', filename: filename});
 const upload = multer({fileFilter, storage: storage});
+const photoPath =path.resolve (__dirname, '../../client/photo-viewer.html');
 
 
 function filename (request, file ,callback) { callback(null, file.originalname) };
@@ -10,7 +13,7 @@ function filename (request, file ,callback) { callback(null, file.originalname) 
 function fileFilter(request, file, callback) {
   if (file.mimetype !== 'image/png') {
     request.fileValidationError = 'Wrong file type';
-    callback (null, false, {Error : 'Wrong file type'})
+    callback (null, false, new Error('Wrong file type'))
   } else {
     callback (null, true);
   }
@@ -22,5 +25,10 @@ router.post('/upload',upload.single('photo'), function (request, response) {
     return response.status(201).json({success: true})
   }
 });
+
+router.get('/photo-viewer', function (request, response) {
+    response.sendFile(photoPath);
+});
+
 
 module.exports = router;
